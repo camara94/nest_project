5b17d0f353d4839e4cbd62cfdafd3a015cf41559
CAMARA Laby Damaro <color.status>
2023-06-30 18:06:17 +0000

    le modèle utilisateur

--- src/models/utilisateur/utilisateur.interface.ts
+++ src/models/utilisateur/utilisateur.interface.ts
@@ -1,7 +1,4 @@
 import { Adresse } from "../adresse/adresse.interface";
-import { Comentaire } from "../comentaire/comentaire.interface";
-import { Commande } from "../commande/commande.interface";
-
 export interface Utilisateur {
     id: number;
 	prenom: string;
